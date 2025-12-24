495484c5226ca8d700734f6ba8e62dd87ab85e98
fubrsmp <scemerworker@gmail.com>
2025-12-24 15:03:02 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const discordjoin = document.getElementById("discordlink");
+const javajoin = document.getElementById("javaip");
+const bedrockjoin = document.getElementById("bedrockip");
+const eaglerjoin = document.getElementById("eaglercraftlink");
+discordjoin.addEventListener("click", function() {
+    window.location.href = "https://discord.gg/JrvGFTauq3";
+});
+javajoin.addEventListener("click", function() {
+    navigator.clipboard.writeText("safety-submission.gl.joinmc.link");
+    javajoin.textContent = "Copied!";
+});
+bedrockjoin.addEventListener("click", function() {
+    navigator.clipboard.writeText("sources-equivalent.gl.at.ply.gg");
+    bedrockjoin.textContent = "Copied!";
+});
+eaglerjoin.addEventListener("click", function() {
+    window.location.href = "/client.html";
+});
